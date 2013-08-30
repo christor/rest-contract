33eb7bdbd07cbc17e43cea4cec81ca629d5a4fa2
abeggs <sunyal@gmail.com>
2013-08-30 10:24:21 -0400

    Includes some very basic tests.  Uses the example from the readme file to ensure that the expected functionality works.

---
+++ src/test/java/org/christor/restcontract/client/ContractClientTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.christor.restcontract.client;
+
+import org.christor.restcontract.Contract;
+import org.christor.restcontract.Request;
+import org.christor.restcontract.Response;
+import org.christor.restcontract.server.ContractServer;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alex
+ */
+public class ContractClientTest {
+
+    @Test
+    public void testNullContract() throws Exception {
+        Contract contract = null;
+        try {
+            ContractClient client = new ContractClient(contract);
+            client.run("http://localhost:5555");
+            fail("Expected a NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @Test
+    public void testMatchingContractWithServerAndClient() throws Exception {
+        Contract contract = new Contract();
+        final String baseUrl = "http://localhost:5555/";
+        contract.rule(Request.get(baseUrl),
+                Response.code(200).body("ThoughtStreams API")
+                .header("Content-Type", "text/plain"));
+        contract.rule(Request.get("foo"),
+                Response.code(200)
+                .body("bar")
+                .header("Content-Type", "text/plain"));
+        contract.rule(Request.get("fooh"),
+                Response.code(200)
+                .body("bar")
+                .header("Content-Type", "text/html"));
+        contract.rule(Request.get("asdfasdfa"),
+                Response.code(404).body("Go away")
+                .header("Content-Type", "text/html"))
+                .then(Request.get("asdfasdfa"),
+                Response.code(404)
+                .body("Go away or I will taunt you a second time")
+                .header("Content-Type", "text/html"));
+
+        ContractServer server = new ContractServer();
+        server.start(baseUrl, contract);
+        ContractClient client = new ContractClient(contract);
+        client.run(baseUrl);
+        server.stop();
+    }
+}
