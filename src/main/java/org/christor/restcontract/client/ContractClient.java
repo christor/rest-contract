21178aca3c97e1faba7820e2c6f786c3d185cc7b
abeggs <sunyal@gmail.com>
2013-08-30 10:23:35 -0400

    Now calls destroy on the Jersey Client that was created in the scope of the method call to release the resources.  Could have used the Lombok @Cleanup but if we had chained Rules it would not clean up until the subsequent calls were finished.

--- src/main/java/org/christor/restcontract/client/ContractClient.java
+++ src/main/java/org/christor/restcontract/client/ContractClient.java
@@ -9,6 +9,8 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.WebResource.Builder;
 import com.sun.jersey.api.client.filter.ClientFilter;
 import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.MultivaluedMap;
 import org.christor.restcontract.BodyMatcher;
 import org.christor.restcontract.Contract;
@@ -20,7 +22,8 @@ import org.christor.restcontract.Rule;
 
 public class ContractClient {
 
-    Contract contract;
+    private Logger logger = Logger.getLogger(getClass().getName());
+    private Contract contract;
 
     public ContractClient(Contract contract) {
         this.contract = contract;
@@ -35,37 +38,44 @@ public class ContractClient {
             }
         }
     }
-
+    
     public void runRule(Rule rule, String baseUrl) throws MalformedURLException, RestContractViolationException {
-        final Client jerseyClient = Client.create();
-        final ResponseHeaderCollectorClientFilter clientFilter = new ResponseHeaderCollectorClientFilter();
+        if (logger.isLoggable(Level.FINE)) {
+            logger.log(Level.FINE, "Running rule {0} on baseUrl {1}", new Object[]{rule, baseUrl});
+        }
         final Request request = rule.getRequest();
         final String urlString = baseUrl + request.getPath().getTextValue();
-        final WebResource resource = jerseyClient.resource(urlString);
-        jerseyClient.addFilter(clientFilter);
-        Builder builder = resource.getRequestBuilder();
-        for (HeaderMatcher matcher : request.getHeaders()) {
-            builder = resource.header(matcher.getKey(), matcher.getValue());
-        }
-        if (request.getBody() != null && request.getBody().getOutputText() != null) {
-            builder.entity(request.getBody().getOutputText());
-        }
+        final Client jerseyClient = Client.create();
         try {
-            builder.method(request.getMethod().getTextValue());
-        } catch (UniformInterfaceException ex) {
-            // non-2xx response codes will cause us to land here
-        }
-        final String responseText = clientFilter.getBody();
-        final Response response = rule.getResponse();
-        if (!response.getStatus().matches(clientFilter.getStatus())) {
-            throw new RestContractViolationException("Error in rule: " + rule + " Unexpected HTTP Status Code. Expected: " + response.getStatus().getValue() + " but got " + clientFilter.getStatus());
-        }
-        BodyMatcher body = response.getBody();
-        if (!body.matches(responseText)) {
-            throw new RestContractViolationException("Unexpected response body. Expected: " + body + " But got: " + responseText);
-        }
-        if (!response.getHeaders().matches(clientFilter.getResponseHeaders())) {
-            throw new RestContractViolationException("Not all required headers found");
+            final ResponseHeaderCollectorClientFilter clientFilter = new ResponseHeaderCollectorClientFilter();
+            final WebResource resource = jerseyClient.resource(urlString);
+            jerseyClient.addFilter(clientFilter);
+            Builder builder = resource.getRequestBuilder();
+            for (HeaderMatcher matcher : request.getHeaders()) {
+                builder = resource.header(matcher.getKey(), matcher.getValue());
+            }
+            if (request.getBody() != null && request.getBody().getOutputText() != null) {
+                builder.entity(request.getBody().getOutputText());
+            }
+            try {
+                builder.method(request.getMethod().getTextValue());
+            } catch (UniformInterfaceException ex) {
+                // non-2xx response codes will cause us to land here
+            }
+            final String responseText = clientFilter.getBody();
+            final Response response = rule.getResponse();
+            if (!response.getStatus().matches(clientFilter.getStatus())) {
+                throw new RestContractViolationException("Error in rule: " + rule + " Unexpected HTTP Status Code. Expected: " + response.getStatus().getValue() + " but got " + clientFilter.getStatus());
+            }
+            BodyMatcher body = response.getBody();
+            if (!body.matches(responseText)) {
+                throw new RestContractViolationException("Unexpected response body. Expected: " + body + " But got: " + responseText);
+            }
+            if (!response.getHeaders().matches(clientFilter.getResponseHeaders())) {
+                throw new RestContractViolationException("Not all required headers found");
+            }
+        } finally {
+            jerseyClient.destroy();
         }
         if (rule.hasNext()) {
             runRule(rule.getNext(), baseUrl);
